ee7dff2b688b0a2e05417b3c46274d8e9d3f7be4
Tyler Ross <tyguy@pop-os.localdomain>
2022-06-22 09:26:25 -0400

    css is irritating but started on home page styling

--- src/components/pages/homePage/homePage.js
+++ src/components/pages/homePage/homePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {SideNav} from '../../sideNav/sideNav'
+import {SideNav} from '../../sideNav/sideNav';
+import '../../../assets/css/homeSection.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     Container,
@@ -12,6 +13,13 @@ export const HomePage = () =>{
     return (
         <>
             <SideNav/>
+            <Container>
+                <div className='homeBG'></div>
+                <Col className='homeContent'>
+                    <div className='homeH1'>Hey I Program</div>
+                    <div className='homeP'>I dont know it's pretty cool I guess</div>
+                </Col>
+            </Container>
         </>
     )
 }
